e25d45c791f97458f6c89aa1681189a97769ebbc
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 14:47:07 +0300

    Initial Commit

---
+++ src/main/java/me/grenadinio/hardtasks/HardTasksPlugin.java
@@ -0,0 +1,17 @@
+package me.grenadinio.hardtasks;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HardTasksPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable(){
+
+    }
+
+}
